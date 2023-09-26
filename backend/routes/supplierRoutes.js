c2613a2156b3856983de88e602ee828f08de4131
Dek Ibrahim Muse <deekibraa@gmail.com>
2023-09-27 02:08:23 +0300

    updated

--- backend/routes/supplierRoutes.js
+++ backend/routes/supplierRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const adminSuppliesController = require('../controllers/adminSuppliesController');
 
 // Define routes for admin supplies
-router.get('/', adminSuppliesController.getAllSupplies);
-router.post('/', adminSuppliesController.createSupply);
-router.put('/:id', adminSuppliesController.updateSupply);
-router.delete('/:id', adminSuppliesController.deleteSupply);
+router.get('all', SuppliesController.getAllSupplies);
+router.post('new', SuppliesController.createSupply);
+router.put('update/:id', SuppliesController.updateSupply);
+router.delete('delete/:id', SuppliesController.deleteSupply);
 
 module.exports = router;
