d0fb2820d1004241e40fd80d5e5c6e5a594e26d3
Dek Ibrahim Muse <deekibraa@gmail.com>
2023-09-27 03:05:31 +0300

    done

--- backend/server/index.js
+++ backend/server/index.js
@@ -5,7 +5,9 @@ const port = 3000;
 
 // Import and use the customer routes
 const customerRoutes = require('../routes/customerRoutes');
-app.use('/api', customerRoutes);
+const userRoute = require('../routes/userRoute');
+app.use('/api/customer', customerRoutes);
+app.use('api/user', userRoute)
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
